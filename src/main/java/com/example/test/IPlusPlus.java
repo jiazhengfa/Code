cd25c692938823770cf4f22c1002f824642c24a5
贾正发 <1163553474@qq.com>
2024-11-27 20:15:48 +0800

    踩坑

---
+++ src/main/java/com/example/test/IPlusPlus.java
@@ -0,0 +1,25 @@
+package com.example.test;
+
+/**
+ * ClassName: IPlusPlus
+ * Package: com.example.test
+ * Description:
+ *
+ * @Author 贾正发
+ * @Create 2024/11/27 20:13
+ * @Version 1.0
+ */
+public class IPlusPlus {
+    public static void main(String[] args)
+    {
+        int i = 1;
+
+        System.out.println("i: "  +   i);
+        System.out.println("++i: "+   ++i);
+        System.out.println("i++: "+   i++);
+        System.out.println("i: "  +     i);
+        System.out.println("--i: "+   --i);
+        System.out.println("i--: "+   i--);
+        System.out.println("i: "  +   i);
+    }
+}
